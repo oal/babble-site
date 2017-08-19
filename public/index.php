dbfb92b3866c87f8b0b435a8f43707647dbcdf7b
Olav Lindekleiv <olav@lindekleiv.com>
2017-08-19 14:50:53 +0200

    More documentation.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once 'vendor/autoload.php';
-
-use Babble\Babble;
+require_once '../vendor/autoload.php';
 
 if (preg_match('/^(\/(static|admin|uploads)(\/|$))/', $_SERVER['REQUEST_URI'])) {
     return false;
 }
 
+use Babble\Babble;
 $babble = new Babble();
 $babble->serve();
